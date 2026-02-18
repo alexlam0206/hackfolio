c005af463701ca07fea42c3a30dc77a551f4e67a
alexlam0206 <nok@pgmiv.com>
2026-02-18 19:37:31 +0800

    feat: add login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import React from 'react';
 import { NavigationMenuItem } from '@/components/ui/navigation-menu';
+import { signIn } from "next-auth/react";
+
+
 function LandingPage() {
   return (
     <div>
@@ -7,7 +12,7 @@ function LandingPage() {
       <main>
         <h1>Hackfolio</h1>
         <p>a linkinbio for Hack Clubbers!</p>
-        <button type="button">Login</button>
+        <button onClick={() => signIn()}>Login with Hack Club Auth</button>
       </main>
     </div>
   );
